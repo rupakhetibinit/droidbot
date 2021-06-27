ab11d1950ff4ff75b564534b28cfc9181effad30
Binit Rupakheti <cruiserponder@protonmail.com>
2021-06-27 17:43:45 +0545

    ready for hosting

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,18 @@
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="droidbot">
-        <h1>DROID BOT</h1>
-      </div>
-      <div className="links">
-        <a href="/">Home</a>
-        <a href="/documentation">Documentation</a>
-        <a href="/INVITEKEY">Invite</a>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className="navbar">
+			<div className="droidbot">
+				<h1>DROID BOT</h1>
+			</div>
+			<div className="links">
+				<a href="/">Home</a>
+				<a href="/https://github.com/rupakhetibinit/droidbot">Documentation</a>
+				<a href="/https://discord.com/oauth2/authorize?client_id=825383004812869652&scope=bot&permissions=379904&fbclid=IwAR0rF8wewZae30gnhEWKcX4RzIgLFNUn0vzd4c9AIDmOu3BUrIm46-NQ2aE">
+					Invite
+				</a>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
