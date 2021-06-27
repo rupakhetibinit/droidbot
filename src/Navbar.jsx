23e24fd15bea131e51ee1922ef22cd188cff29c3
Binit Rupakheti <cruiserponder@protonmail.com>
2021-06-27 17:56:28 +0545

    fixed documentation and Invite redirects not working

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
 			</div>
 			<div className="links">
 				<a href="/">Home</a>
-				<a href="/https://github.com/rupakhetibinit/droidbot">Documentation</a>
-				<a href="/https://discord.com/oauth2/authorize?client_id=825383004812869652&scope=bot&permissions=379904&fbclid=IwAR0rF8wewZae30gnhEWKcX4RzIgLFNUn0vzd4c9AIDmOu3BUrIm46-NQ2aE">
+				<a href="https://github.com/rupakhetibinit/droidbot">Documentation</a>
+				<a href="https://discord.com/oauth2/authorize?client_id=825383004812869652&scope=bot&permissions=379904&fbclid=IwAR0rF8wewZae30gnhEWKcX4RzIgLFNUn0vzd4c9AIDmOu3BUrIm46-NQ2aE">
 					Invite
 				</a>
 			</div>
